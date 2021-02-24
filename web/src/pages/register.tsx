5efb3251cbcbc76fba1b695f24d2b12eef29002a
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2021-02-24 17:05:31 +0900

    set up ssr for home page & display posts

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import { Form, Formik } from "formik";
-import {
-  Box,
-  Button,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Input,
-} from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { useRegisterMutation } from "../generated/graphql";
 import { toErrorMap } from "../../utils/toErrorMap";
 import { useRouter } from "next/router";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../../utils/createUrqlClient";
 
 interface registerProps {}
 
@@ -63,4 +58,4 @@ export const Register: React.FC<registerProps> = ({}) => {
   );
 };
 
-export default Register;
+export default withUrqlClient(createUrqlClient)(Register);
