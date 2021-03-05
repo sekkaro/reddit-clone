887aa8583eb4b23cb4083acc1f776847a31bea67
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2021-03-05 15:47:05 +0900

    more urql pagination

--- web/src/generated/graphql.tsx
+++ web/src/generated/graphql.tsx
@@ -42,6 +42,7 @@ export type Post = {
   creatorId: Scalars['Float'];
   createdAt: Scalars['String'];
   updatedAt: Scalars['String'];
+  textSnippet: Scalars['String'];
 };
 
 export type User = {
@@ -240,7 +241,7 @@ export type PostsQuery = (
   { __typename?: 'Query' }
   & { posts: Array<(
     { __typename?: 'Post' }
-    & Pick<Post, 'id' | 'title' | 'createdAt' | 'updatedAt'>
+    & Pick<Post, 'id' | 'title' | 'textSnippet' | 'createdAt' | 'updatedAt'>
   )> }
 );
 
@@ -351,6 +352,7 @@ export const PostsDocument = gql`
   posts(cursor: $cursor, limit: $limit) {
     id
     title
+    textSnippet
     createdAt
     updatedAt
   }
