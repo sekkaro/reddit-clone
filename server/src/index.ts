7b8eaccc7d77c9080394e946852175a9283978f8
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2021-03-08 16:26:58 +0900

    many to many relationship with updoot entity between post and user

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,6 +16,7 @@ import { Post } from "./entities/Post";
 import { User } from "./entities/User";
 import Config from "./config";
 import path from "path";
+import { Updoot } from "./entities/Updoot";
 
 const main = async () => {
   const conn = await createConnection({
@@ -26,7 +27,7 @@ const main = async () => {
     logging: true,
     synchronize: true,
     migrations: [path.join(__dirname, "./migrations/*")],
-    entities: [Post, User],
+    entities: [Post, User, Updoot],
   });
   await conn.runMigrations();
 
